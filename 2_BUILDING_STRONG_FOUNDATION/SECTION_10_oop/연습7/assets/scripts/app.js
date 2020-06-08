d7c6fd67dc4810d3da9fd2d37e26bc573fe8c4c2
sliverz6 <52848569+sliverz6@users.noreply.github.com>
2020-06-08 22:59:48 +0900

    script

---
+++ 2_BUILDING_STRONG_FOUNDATION/SECTION_10_oop/연습7/assets/scripts/app.js
@@ -0,0 +1,18 @@
+const products = [
+    { 
+        title: 'A Pillow', 
+        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51gCgsHQwwL._AC_SX522_.jpg',
+        price: 19.99,
+        description: 'A soft pillow!'
+    },
+    { 
+        title: 'A Carpet', 
+        imageUrl: 'https://ae01.alicdn.com/kf/Hb6ed5de492a0424e88f90ab64d3f1633j.jpg',
+        price: 89.99,
+        description: 'A carpet which you might like - or not.'
+    },
+];
+
+const productList = {
+    
+};
